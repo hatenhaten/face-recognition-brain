12dc0e78896e74716f7e561428b489850ba1d8bc
Kallum Tolkien <kallumtolkien@gmail.com>
2022-01-23 23:34:03 +0000

    Update and style the Logo component

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import Tilt from 'react-parallax-tilt';
+import brain from './brain2.png';
 import './Logo.css';
 
 const Logo = () => {
 	return (
 		<div className='ma4 mt0'>
-			<Tilt>
+			<Tilt tiltMaxAngleX={40} tiltMaxAngleY={40}>
 				<div className='br2 shadow-2 tilt'>
-					<h1>👀</h1>
+					<div className='pa3'><img style={{paddingTop: '5px'}} src={brain} alt="brain"></img></div>
 				</div>
 			</Tilt>
 		</div>
