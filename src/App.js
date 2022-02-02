022d51eb445401b090b6c12de34ce0184d5f6a9c
Kallum Tolkien <kallumtolkien@gmail.com>
2022-02-02 22:44:47 +0000

    Seperates Particles Options as a standalone variable

--- src/App.js
+++ src/App.js
@@ -6,6 +6,84 @@ import Rank from './components/Rank/Rank';
 import Particles from 'react-tsparticles';
 import './App.css';
 
+const particlesOptions = {
+	background: {
+		color: {
+			value: 'transparent',
+		},
+	},
+	fpsLimit: 60,
+	interactivity: {
+		events: {
+			onClick: {
+				enable: true,
+				mode: 'push',
+			},
+			onHover: {
+				enable: false,
+				mode: 'repulse',
+			},
+			resize: true,
+		},
+		modes: {
+			bubble: {
+				distance: 400,
+				duration: 2,
+				opacity: 0.8,
+				size: 40,
+			},
+			push: {
+				quantity: 4,
+			},
+			repulse: {
+				distance: 200,
+				duration: 0.4,
+			},
+		},
+	},
+	particles: {
+		color: {
+			value: '#ffffff',
+		},
+		links: {
+			color: '#ffffff',
+			distance: 150,
+			enable: true,
+			opacity: 0.5,
+			width: 1,
+		},
+		collisions: {
+			enable: true,
+		},
+		move: {
+			direction: 'none',
+			enable: true,
+			outMode: 'bounce',
+			random: false,
+			speed: 1,
+			straight: false,
+		},
+		number: {
+			density: {
+				enable: true,
+				area: 800,
+			},
+			value: 80,
+		},
+		opacity: {
+			value: 0.1,
+		},
+		shape: {
+			type: 'circle',
+		},
+		size: {
+			random: true,
+			value: 5,
+		},
+	},
+	detectRetina: true,
+};
+
 class App extends Component {
 	render() {
 		const particlesInit = main => {
@@ -19,89 +97,7 @@ class App extends Component {
 		};
 		return (
 			<div className='App'>
-				<Particles
-					className='particles'
-					id='tsparticles'
-					init={particlesInit}
-					loaded={particlesLoaded}
-					options={{
-						background: {
-							color: {
-								value: 'transparent',
-							},
-						},
-						fpsLimit: 60,
-						interactivity: {
-							events: {
-								onClick: {
-									enable: true,
-									mode: 'push',
-								},
-								onHover: {
-									enable: false,
-									mode: 'repulse',
-								},
-								resize: true,
-							},
-							modes: {
-								bubble: {
-									distance: 400,
-									duration: 2,
-									opacity: 0.8,
-									size: 40,
-								},
-								push: {
-									quantity: 4,
-								},
-								repulse: {
-									distance: 200,
-									duration: 0.4,
-								},
-							},
-						},
-						particles: {
-							color: {
-								value: '#ffffff',
-							},
-							links: {
-								color: '#ffffff',
-								distance: 150,
-								enable: true,
-								opacity: 0.5,
-								width: 1,
-							},
-							collisions: {
-								enable: true,
-							},
-							move: {
-								direction: 'none',
-								enable: true,
-								outMode: 'bounce',
-								random: false,
-								speed: 1,
-								straight: false,
-							},
-							number: {
-								density: {
-									enable: true,
-									area: 800,
-								},
-								value: 80,
-							},
-							opacity: {
-								value: 0.1,
-							},
-							shape: {
-								type: 'circle',
-							},
-							size: {
-								random: true,
-								value: 5,
-							},
-						},
-						detectRetina: true,
-					}}
-				/>
+				<Particles className='particles' id='tsparticles' init={particlesInit} loaded={particlesLoaded} options={particlesOptions} />
 				<Navigation />
 				<Logo />
 				<Rank />
