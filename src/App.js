e832a0fb490779454a9735737ac5c7f93794fc74
Kallum Tolkien <kallumtolkien@gmail.com>
2022-02-11 10:38:30 +0000

    Adds onSubmit listener to button of ImageLinkForm which logs a click to the console

--- src/App.js
+++ src/App.js
@@ -104,6 +104,10 @@ class App extends Component {
 		console.log(event.target.value);
 	};
 
+	onButtonSubmit = () => {
+		console.log('click');
+	};
+
 	render() {
 		return (
 			<div className='App'>
@@ -111,7 +115,7 @@ class App extends Component {
 				<Navigation />
 				<Logo />
 				<Rank />
-				<ImageLinkForm onInputChange={this.onInputChange} />
+				<ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit} />
 				{/*
         <FaceRecognition /> */}
 			</div>
