87960146089ab0cf571034ebe3c6dee3a384fe61
Kallum Tolkien <kallumtolkien@gmail.com>
2022-02-28 12:36:40 +0000

    Add sign in, sign out and register components with basic functionality

--- src/App.js
+++ src/App.js
@@ -1,139 +1,142 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 import SignIn from './components/SignIn/SignIn';
+import Register from './components/Register/Register';
 import Rank from './components/Rank/Rank';
 import Particles from 'react-tsparticles';
 import Clarifai from 'clarifai';
 import './App.css';
 
 const app = new Clarifai.App({
-	apiKey: 'a68e8f272db14527a8d91a6b0dd54a3a',
+  apiKey: 'a68e8f272db14527a8d91a6b0dd54a3a',
 });
 
 const particlesOptions = {
-	background: {
-		color: {
-			value: 'transparent',
-		},
-	},
-	fpsLimit: 60,
-	interactivity: {
-		events: {
-			onClick: {
-				enable: true,
-				mode: 'push',
-			},
-			onHover: {
-				enable: false,
-				mode: 'repulse',
-			},
-			resize: true,
-		},
-		modes: {
-			bubble: {
-				distance: 400,
-				duration: 2,
-				opacity: 0.8,
-				size: 40,
-			},
-			push: {
-				quantity: 4,
-			},
-			repulse: {
-				distance: 200,
-				duration: 0.4,
-			},
-		},
-	},
-	particles: {
-		color: {
-			value: '#ffffff',
-		},
-		links: {
-			color: '#ffffff',
-			distance: 150,
-			enable: true,
-			opacity: 0.5,
-			width: 1,
-		},
-		collisions: {
-			enable: true,
-		},
-		move: {
-			direction: 'none',
-			enable: true,
-			outMode: 'bounce',
-			random: false,
-			speed: 1,
-			straight: false,
-		},
-		number: {
-			density: {
-				enable: true,
-				area: 800,
-			},
-			value: 80,
-		},
-		opacity: {
-			value: 0.1,
-		},
-		shape: {
-			type: 'circle',
-		},
-		size: {
-			random: true,
-			value: 5,
-		},
-	},
-	detectRetina: true,
+  background: {
+    color: {
+      value: 'transparent',
+    },
+  },
+  fpsLimit: 60,
+  interactivity: {
+    events: {
+      onClick: {
+        enable: true,
+        mode: 'push',
+      },
+      onHover: {
+        enable: false,
+        mode: 'repulse',
+      },
+      resize: true,
+    },
+    modes: {
+      bubble: {
+        distance: 400,
+        duration: 2,
+        opacity: 0.8,
+        size: 40,
+      },
+      push: {
+        quantity: 4,
+      },
+      repulse: {
+        distance: 200,
+        duration: 0.4,
+      },
+    },
+  },
+  particles: {
+    color: {
+      value: '#ffffff',
+    },
+    links: {
+      color: '#ffffff',
+      distance: 150,
+      enable: true,
+      opacity: 0.5,
+      width: 1,
+    },
+    collisions: {
+      enable: true,
+    },
+    move: {
+      direction: 'none',
+      enable: true,
+      outMode: 'bounce',
+      random: false,
+      speed: 1,
+      straight: false,
+    },
+    number: {
+      density: {
+        enable: true,
+        area: 800,
+      },
+      value: 80,
+    },
+    opacity: {
+      value: 0.1,
+    },
+    shape: {
+      type: 'circle',
+    },
+    size: {
+      random: true,
+      value: 5,
+    },
+  },
+  detectRetina: true,
 };
 
-const particlesInit = main => {
-	console.log(main);
+const particlesInit = (main) => {
+  console.log(main);
 };
 
-const particlesLoaded = container => {
-	console.log(container);
+const particlesLoaded = (container) => {
+  console.log(container);
 };
 
 class App extends Component {
-	constructor() {
-		super();
-		this.state = {
-			input: '',
-			imageUrl: '',
-			box: {},
-		};
-	}
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+      imageUrl: '',
+      box: {},
+      route: 'signin',
+      isSignedIn: false,
+    };
+  }
 
-	//prettier-ignore
-	calculateFaceLocation = data => {
-		const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
-		const image = document.getElementById('input_image');
-		const width = Number(image.width);
-		const height = Number(image.height);
-		return {
-			leftCol: clarifaiFace.left_col * width,
-			topRow: clarifaiFace.top_row * height,
-			rightCol: width - (clarifaiFace.right_col * width),
-			bottomRow: height - (clarifaiFace.bottom_row * height),
-		};
-	};
+  calculateFaceLocation = (data) => {
+    const clarifaiFace =
+      data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById('input_image');
+    const width = Number(image.width);
+    const height = Number(image.height);
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - clarifaiFace.right_col * width,
+      bottomRow: height - clarifaiFace.bottom_row * height,
+    };
+  };
 
-	displayFaceBox = box => {
-		console.log(box);
-		this.setState({box: box});
-	};
+  displayFaceBox = (box) => {
+    console.log(box);
+    this.setState({ box: box });
+  };
 
-	onInputChange = event => {
-		this.setState({input: event.target.value});
-	};
+  onInputChange = (event) => {
+    this.setState({ input: event.target.value });
+  };
 
-	//prettier-ignore
-	onButtonSubmit = () => {
+  //prettier-ignore
+  onButtonSubmit = () => {
 		this.setState({imageUrl: this.state.input});
 		app.models
       .predict(
@@ -143,32 +146,50 @@ class App extends Component {
       .catch(err => console.log(err));
 	};
 
-	//prettier-ignore
-	render() {
-		return (
-			<div className='App'>
-				<Particles 
-          className='particles' 
-          id='tsparticles' 
-          init={particlesInit} 
-          loaded={particlesLoaded} 
-          options={particlesOptions} 
+  onRouteChange = (route) => {
+    if (route === 'signout') {
+      this.setState({ isSignedIn: false });
+    } else if (route === 'home') {
+      this.setState({ isSignedIn: true });
+    }
+    this.setState({ route: route });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <Particles
+          className="particles"
+          id="tsparticles"
+          init={particlesInit}
+          loaded={particlesLoaded}
+          options={particlesOptions}
         />
-				<Navigation />
-        <SignIn />
-				<Logo />
-				<Rank />
-				<ImageLinkForm 
-          onInputChange={this.onInputChange} 
-          onButtonSubmit={this.onButtonSubmit} 
+        <Navigation
+          isSignedIn={this.state.isSignedIn}
+          onRouteChange={this.onRouteChange}
         />
-				<FaceRecognition
-          box={this.state.box}
-          imageUrl={this.state.imageUrl} 
-        />
-			</div>
-		);
-	}
+        {this.state.route === 'home' ? (
+          <div>
+            <Logo />
+            <Rank />
+            <ImageLinkForm
+              onInputChange={this.onInputChange}
+              onButtonSubmit={this.onButtonSubmit}
+            />
+            <FaceRecognition
+              box={this.state.box}
+              imageUrl={this.state.imageUrl}
+            />
+          </div>
+        ) : this.state.route === 'signin' ? (
+          <SignIn onRouteChange={this.onRouteChange} />
+        ) : (
+          <Register onRouteChange={this.onRouteChange} />
+        )}
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,11 +1,20 @@
 import React from 'react';
 
-const Navigation = () => {
-  return (
-    <nav style={{display: 'flex', justifyContent: 'flex-end'}}>
-      <p className='f3 link dim black underline pa3 pointer'>Sign Out</p>
-    </nav>
-  )
-}
+const Navigation = ({ onRouteChange, isSignedIn }) => {
+  if (isSignedIn) {
+    return (
+      <nav style={{ display: 'flex', justifyContent: 'flex-end' }}>
+        <p
+          onClick={() => onRouteChange('signout')}
+          className="f3 link dim black underline pa3 pointer"
+        >
+          Sign Out
+        </p>
+      </nav>
+    );
+  } else {
+    return <div style={{ padding: '50px' }}></div>;
+  }
+};
 
 export default Navigation;
